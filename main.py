207eefa1fc4a649d04f60788c80800733a3f8172
Birk Karlsen-Bæck <birk.karlsen.baeck@gmail.com>
2021-04-06 14:46:27 +0200

    Finished the time iteration function and started on the solver. Still only the 1D case.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import numpy as np
 import scipy as sp
+import scipy.sparse as sps
+import scipy.sparse.linalg as spsl
+import scipy.linalg as spl
 import matplotlib.pyplot as plt
+import time
 
 # Physical constants for the project
 
@@ -9,8 +13,8 @@ V_a = 1         # ---
 R = 1           # Ideal gas constant
 v_0 = 1         # Convection parameter
 ro = 1          # Density of water
-g = 9.81        # gravitational akseleration
-p_ref = 1       # Refrence pressure at ocean surface
+g = 9.81        # gravitational acceleration
+p_ref = 1       # Reference pressure at ocean surface
 h = 100         # Ocean depth
 
 # Computational constants for the project
@@ -41,7 +45,9 @@ def T_afo_z(z, T_ref, A):
 
 
 # Functions related to the numerical computations
-def initialize_L(alpha, Gamma, K, Km):
+
+# These functions are for the 1D case. Will generalize to 2D and 3D later.
+def initialize_L1D(alpha, Gamma, K, Km):
     du = - alpha / 4 * Km[:-1] - alpha * K[:-1]
     dl = alpha / 4 * Km[1:] - alpha * K[1:]
     d = 1 + 2 * alpha * K
@@ -50,9 +56,9 @@ def initialize_L(alpha, Gamma, K, Km):
     dl[-1] = -2 * alpha * K[-1]
     d[0] += Gamma
 
-    return sp.sparse.diags([du, d, dl], [1, 0, -1])
+    return sps.diags([du, d, dl], [1, 0, -1])
 
-def initialize_R(alpha, Gamma, K, Km):
+def initialize_R1D(alpha, Gamma, K, Km):
     du = alpha / 4 * Km[:-1] + alpha * K[:-1]
     dl = - alpha / 4 * Km[1:] + alpha * K[1:]
     d = 1 - 2 * alpha * K
@@ -61,8 +67,58 @@ def initialize_R(alpha, Gamma, K, Km):
     dl[-1] = 2 * alpha * K[-1]
     d[0] -= Gamma
 
-    return sp.sparse.diags([du, d, dl], [1, 0, -1])
+    return sps.diags([du, d, dl], [1, 0, -1])
 
-def time_iteration(L, R, S, Ci):
-    V = np.matmul(R, Ci) + (1/2) * (S[:,i] + S[:,i+1])
+def time_iteration1D(L, R, S, Ci, i):
+    V = np.matmul(R.toarray(), Ci) + (1/2) * (S.toarray()[:,i] + S.toarray()[:,i+1])
+    return spsl.spsolve(L.tocsr(), V)
 
+def solver1D(kw, K_func, C0_func, S_func, dz, dt, depth, totalTime):
+    # Make the environment to simulate
+    Nz = int(depth//dz)
+    Nt = int(totalTime//dt)
+    z = np.linspace(0, depth, Nz)
+    t = np.linspace(0, totalTime, Nt)
+
+    # Turn the initial distribution and K into arrays
+    K = K_func(z)
+    S = sps.lil_matrix((Nz, Nt))
+    S[0,:] = S_func(t)
+    C = np.zeros((Nz, Nt))
+    C[:,0] = C0_func(z)
+    Km = np.roll(K, -1) - np.roll(K, 1)
+
+    # Compute some important coefficients for the simulation
+    alpha = dt / (2 * dz**2)
+    Gamma = 2 * alpha * kw * dz * (1 - (-(3/2) * K[0] + 2*K[1] - (1/2)*K[2])/(2*K[0]))
+
+    # The matrices for the time-iterations
+    L = initialize_L1D(alpha, Gamma, K, Km)
+    R = initialize_R1D(alpha, Gamma, K, Km)
+
+    # The actual time-iteration
+    for i in range(Nt-1):
+        C[:,i+1] = time_iteration1D(L, R, S, C[:,i], i)
+
+    return C, z, t, K
+
+kw = 0
+depth = 100
+dz = 0.01
+dt = 0.1
+totalTime = 100
+
+def K1(z):
+    return np.ones(np.shape(z))
+
+def C01(z):
+    return np.exp(-(z-depth/2)**2 / 2)
+
+def S1(t):
+    return np.ones(np.shape(t))
+
+C, z, t, K = solver1D(kw, K1, K1, S1, dz, dt, depth, totalTime)
+
+plt.plot(z, C[:,0])
+plt.plot(z,C[:,1])
+plt.show()
