a1d85a45940dca41cfb1aa41a9a7296af24cd0f4
Birk Karlsen-Bæck <birk.karlsen.baeck@gmail.com>
2021-04-10 19:07:05 +0200

    Added the 2D version of the solver

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import numpy as np
-import scipy as sp
 import scipy.sparse as sps
 import scipy.sparse.linalg as spsl
 import scipy.linalg as spl
 import matplotlib.pyplot as plt
 import time
+from tqdm import tqdm
+from numba import jit, prange
+import numba_scipy
 
 # Physical constants for the project
 
@@ -69,10 +71,11 @@ def initialize_R1D(alpha, Gamma, K, Km):
 
     return sps.diags([du, d, dl], [1, 0, -1])
 
+
 def time_iteration1D(L, R, S, Ci, i):
     V = np.matmul(R.toarray(), Ci) + (1/2) * (S.toarray()[:,i] + S.toarray()[:,i+1])
-    if i == 0: print(V)
     return spsl.spsolve(L.tocsr(), V)
+
 
 def solver1D(kw, K_func, C0_func, S_func, dz, dt, depth, totalTime):
     # Make the environment to simulate
@@ -104,6 +107,99 @@ def solver1D(kw, K_func, C0_func, S_func, dz, dt, depth, totalTime):
 
     return C, z, t, K
 
+# Now for the 2D case. We will now add convection into the mix.
+# We assume a square lattice of points in the x and z directions and get.
+
+def initialize_L_and_R2D(Nx, Nz, vx, vz, dt, dx, D, K):
+    N = Nx * Nz
+    L = sps.lil_matrix((N,N))
+    R = sps.lil_matrix((N,N))
+
+    for i in prange(Nx):
+        for j in range(Nz):
+            # The gamma-coefficients for the matrices
+            Km = K[j] * dt / (2 * dx**2)
+            dK = D[j] * dt / (8 * dz**2)
+            vxm = vx[i,j] * dt / (4 * dx)
+            vzm = vz[i, j] * dt / (4 * dx)
+            dvx = (vx[(i+1)%Nx, j] - vx[(i-1)%Nx, j]) * dt / (4 * dx)
+            dvz = (vz[i, (j+1)%Nz] - vz[i, (j-1)%Nx]) * dt / (4 * dx)
+
+            n = j + Nz * i
+
+            # Main diagonal
+            L[n,n] = 1 + 4*Km + dvx + dvz
+            R[n,n] = 1 - 4*Km - dvx - dvz
+
+            # The superdiagonal
+            L[n,(n+1)%Nz + Nz * i] = - dK - Km + vzm
+            R[n,(n+1)%Nz + Nz * i] = dK + Km - vzm
+
+            # The subdiagonal
+            L[n,(n-1)%Nz + Nz * i] = - dK - Km - vzm
+            R[n,(n-1)%Nz + Nz * i] = dK + Km + vzm
+
+            # The supersuperdiagonal
+            L[n,(n+Nz)%N] = - Km + vxm
+            R[n,(n+Nz)%N] = Km - vxm
+
+            # The subsubdiagonal
+            L[n,(n-Nz)%N] = - Km - vxm
+            R[n,(n-Nz)%N] = Km + vxm
+
+    # Last part here is for boundary conditions, which I will add in later.
+
+    return L.todia(), R.todia()
+
+
+def initialize_St(S_func, t, N, Nt, Nz, Nx):
+    S = sps.lil_matrix((N, Nt))
+    for i in range(Nt):
+        S[0::Nz,i] = S_func(t[i]) * np.ones(Nx)
+    return S
+
+
+def time_iteration2D(L, R, Ci, S, i):
+    V = np.matmul(R.toarray(), Ci) + (1/2) * (S.toarray()[:,i] + S.toarray()[:,i+1])
+    return spsl.spsolve(L.tocsr(), V)
+
+
+def solver2D(kw, K_func, C0_func, S_func, dx, dt, depth, width, totalTime, vx, vz):
+    Nz = int(depth // dx)
+    Nx = int(width // dx)
+    Nt = int(totalTime // dt)
+    N = Nx * Nz
+
+    x = np.linspace(0,width, Nx)
+    z = np.linspace(0, depth, Nz)
+    t = np.linspace(0, totalTime, Nt)
+
+    K = K_func(z)
+    S = initialize_St(S_func, t, N, Nt, Nz, Nx)
+    C = np.zeros((N, Nt))
+    C[:,0] = C0_func(x,z)
+    D = np.roll(K, -1) - np.roll(K, 1)
+
+    alpha = dt / (2 * dx**2)
+    Gamma = 2 * alpha * kw * dx * (1 - (-(3/2) * K[0] + 2*K[1] - (1/2)*K[2])/(2*K[0]))
+
+    print("Initializing L and R...")
+    L, R = initialize_L_and_R2D(Nx, Nz, vx, vz, dt, dx, D, K)
+    S = 2 * Gamma * S
+
+    print("Simulating...")
+    for i in tqdm(range(Nt-1)):
+        C[:,i+1] = time_iteration2D(L, R, C[:,i], S, i)
+
+    return C, z, x, t, K
+
+
+def convert_1D_to_2D(C, Nx, Nz):
+    newC = np.zeros((Nx, Nz))
+    for i in range(Nx):
+        newC[i,:] = C[(i)*Nz:(i+1)*Nz]
+    return newC
+
 # This is just a test run to check that the solver for the 1D case is working as intended.
 kw = 0
 depth = 100
@@ -120,8 +216,29 @@ def C01(z):
 def S1(t):
     return np.ones(np.shape(t))
 
-C, z, t, K = solver1D(kw, K1, C01, S1, dz, dt, depth, totalTime)
+# This is a test run of the 2D code to see if it is working as intended.
 
-plt.plot(z, C[:,0])
-plt.plot(z,C[:,-1])
+kw = 0
+depth = 10
+width = 10
+dx = 0.1
+dt = 0.1
+totalTime = 10
+Nx = int(width//dx)
+Nz = int(depth//dx)
+vx = np.zeros((Nx, Nz))
+vz = np.zeros((Nx, Nz))
+
+def C02D(x,z):
+    C0 = np.zeros(np.shape(x)[0] * np.shape(z)[0])
+    Nz = np.shape(z)[0]
+    for i in range(np.shape(x)[0]):
+        C0[i * Nz:(i+1)*Nz] = np.exp(-(1/2) * ((x[i]-width/2)**2 + (z-depth/2)**2))
+    return C0
+
+
+C, z, x, t, K = solver2D(kw, K1, C02D, S1, dx, dt, depth, width, totalTime, vx, vz)
+
+plt.imshow(convert_1D_to_2D(C[:,-1], Nx, Nz))
+plt.colorbar()
 plt.show()
